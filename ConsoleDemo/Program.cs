5b080db0a349a00285c806202bd400b96e851c2a
Kingcean Tuan <royaljinchen@msn.com>
2026-09-09 15:13:03 +0800

    Update references.

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using Trivial.CommandLine;
+
+var console = StyleConsole.Default;
+console.WriteLine(new LinearGradientConsoleStyle(null, Color.FromArgb(15, 250, 250), Color.FromArgb(85, 168, 255))
+{
+    Bold = true
+}, "Demo of Trivial CommandLine");
+console.WriteLine();
+var dispatcher = new CommandDispatcher();
+dispatcher.Register<SelectionCli>("select");
+dispatcher.Register<ProgressCli>("progress");
+
+await dispatcher.ProcessOrSelectAsync();
---
+++ ConsoleDemo/README.md
@@ -0,0 +1,1 @@
+# Demo of Trivial CommandLine
